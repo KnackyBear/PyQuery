b5b4a5ffadf0025d28cc885e369bea819420163f
Julien Vinet <julien@vinet.dev>
2022-07-21 00:06:38 +0200

    Fixes

--- query.py
+++ query.py
@@ -11,10 +11,12 @@ class Query:
         self.request = None
         self.vars = []
         self.hasHeaders = True
+        self.verbose = False
+        self.method = "GET"
 
     def _getopt(self):
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hc:r:", ["help", "csv=", "request=", "no-header"])
+            opts, args = getopt.getopt(sys.argv[1:], "hc:r:m:v", ["help", "csv=", "request=", "method=", "no-header","verbose"])
         except getopt.GetoptError as err:
             print(err)
             self.usage()
@@ -28,6 +30,10 @@ class Query:
                 self.csv = a
             elif o in ("-r", "--request"):
                 self.request = a
+            elif o in ("-m", "--method"):
+                self.request = o
+            elif o in ("-v", "--verbose"):
+                self.verbose = True
             elif o == "--no-header":
                 self.hasHeaders = False
             else:
@@ -52,14 +58,35 @@ class Query:
             print("       https://duckduckgo.com/?q={myheader}, where {myheader} is the variable used in the column of the csv file with the header 'myheader'.")
             sys.exit(3)
 
+        if self.method not in ("GET", "POST", "PUT", "DELETE", "HEAD"):
+            print("  [!] Method unkown. Work only with GET, POST, DELETE or HEAD. Buy me a coffee if you want more ;-)")
+            sys.exit(3)
+
     def _get_vars_from_request(self):
         r1 = re.findall(r"{[A-Za-z0-9]+}", self.request)
         r2 = [rex[1:-1] for rex in r1]
-        print("  [*] Variables found in request : %s" % r2)
+        if self.verbose:
+            print(f"  [*] Variables found in request : {r2}")
         self.vars = r2
 
     def usage(self):
-        print("Usage : python query.py --csv=/path/to/csv --request=your_formatted_request")
+        print("Usage : python query.py --csv=/path/to/csv --request=your_formatted_request [--method=GET|POST|PUT|DELETE|HEAD] [--no-header] [--verbose]")
+
+    def _request(self, url):
+        if self.method == "GET":
+            r = requests.get(url)
+        if self.method == "POST":
+            r = requests.post(url)
+        if self.method == "PUT":
+            r = requests.put(url)
+        if self.method == "DELETE":
+            r = requests.delete(url)
+        if self.method == "HEAD":
+            r = requests.head(url)
+        if self.verbose:
+            print(f"  [-] {url} =>  {r.status_code} : {r.text}")
+        else:
+            print(f"  [-] {url} =>  {r.status_code}")
 
     def execute(self):
         self._getopt()
@@ -79,23 +106,19 @@ class Query:
                     self.vars.sort()
                     if sorted_headers != self.vars:
                         print("  [!] Headers from csv file and headers from request are different !")
-                        print("  [-] CSV : %s " % sorted_headers)
-                        print("  [-] Vars : %s" % self.vars)
+                        print(f"  [-] CSV : {sorted_headers} ")
+                        print(f"  [-] Vars : {self.vars}")
                         sys.exit(4)
                     else:
                         for row in inputs:
                             fmt = {headers[i]:row[i] for i in range(len(headers))}
-                            print("-- %s" % fmt)
-                            print("-- %s" % self.request)
-                            req = self.request.format(fmt)
-                            print(" [-] %s" % req)
+                            url = self.request.format_map(fmt)
+                            self._request(url)
             else:
                 for row in inputs:
-                    fmt= "'"+"','".join(row)+"'"
-                    print("-- %s" % fmt)
-                    print("-- %s" % self.request)
-                    req = self.request.format(eval(fmt))
-                    print(" [-] %s" % req)
+                    fmt = ["'"+row[i]+"'" for i in range(len(row))]
+                    url = self.request.format(*fmt)
+                    self._request(url)
 
 def main():
     query = Query()
---
+++ readme.md
@@ -0,0 +1,92 @@
+# PyQuery
+
+A simple multi HTTP Request based on CSV file.
+
+## Pre-requisites
+
+```
+pip install -r requirement.txt
+```
+
+## How to use
+
+```
+python query.py --csv=/path/to/csv --request="your_formatted_request" --no-header --verbose --method=GET
+```
+
+``--csv`` ``-c``<br/>
+Path to the CSV file. This file is use during the formatting process. See below for more details.
+ <br/>
+``--request`` ``-r``<br/>
+Your HTTP request, formatted with your variable (ie {0} {1} ... or ... {var1} {var2} ...). See below for more details about how to format your request.
+ <br/>
+``--no-header``<br/>
+Specify that your CSV file have no header.
+ <br/>
+``--verbose``<br/>
+Add response content to the ouput (default, only status code)
+ <br/>
+``--method``<br/>
+Choose the method of your http request. Only GET, POST, PUT, DELETE and HEAD are accepted. Default is GET.
+
+## How to format the HTTP request.
+
+### With no-header option
+
+The CSV does not contains any header (first line in the file).
+Each row is used to generate a new HTTP request.
+
+You have to use ``{0}`` ``{1}`` ``{2}``... in your request to attach variable from CSV  (the number relates to column in file)
+
+_Example :_
+
+```
+https://www.google.com/search?q={0}
+```
+
+With the CSV :
+
+|     |
+| --- |
+| moo |
+| foo |
+| doo |
+
+will generate the following requests :
+
+| Result |
+| --- |
+| https://www.google.com/search?q=moo |
+| https://www.google.com/search?q=foo |
+| https://www.google.com/search?q=doo |
+
+*Warning :* You can have more column in your CSV than number of variables in your request. *Reverse is not possible !*
+
+### Without no-header option
+
+The CSV contains header row (first line of the file).
+The first row is used to check consistency between CSV and the formatted request. You must have the same number of variables and name.
+
+You have to use ``{header1}`` ``{header2}`` ``{header3}``... in your request to attach variable from CSV  (the name of the variable relates to each headers in the file)
+
+_Example :_
+
+```
+https://www.google.com/search?q={header1}
+```
+
+With the CSV :
+
+| header1 |
+| --- |
+| moo |
+| foo |
+| doo |
+
+will generate the following requests :
+
+| Result |
+| --- |
+| https://www.google.com/search?q=moo |
+| https://www.google.com/search?q=foo |
+| https://www.google.com/search?q=doo |
